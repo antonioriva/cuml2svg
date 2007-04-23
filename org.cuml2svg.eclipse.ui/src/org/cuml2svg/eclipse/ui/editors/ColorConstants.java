849c8c45721a7402c14720e8ab36074b50600bda
antonio.riva <antonio.riva@d69d1d93-342c-0410-915e-650e88db52df>
2007-04-23 17:05:50 +0000

    Initial release

---
+++ org.cuml2svg.eclipse.ui/src/org/cuml2svg/eclipse/ui/editors/ColorConstants.java
@@ -0,0 +1,12 @@
+package org.cuml2svg.eclipse.ui.editors;
+
+import org.eclipse.swt.graphics.RGB;
+
+public final class ColorConstants {
+	static RGB KEYWORD = new RGB(50, 50, 50);
+	static RGB COMMENT = new RGB(128, 128, 128);
+	static RGB STRING = new RGB(0, 0, 128);
+	static RGB NUMBER = new RGB(0, 255, 128);
+	static RGB BRACHET = new RGB(255, 0, 0);
+	static RGB DEFAULT = new RGB(0, 0, 0);
+}
---
+++ org.cuml2svg.eclipse.ui/src/org/cuml2svg/eclipse/ui/editors/ModelEditor.java
@@ -0,0 +1,51 @@
+package org.cuml2svg.eclipse.ui.editors;
+
+
+import java.io.StringWriter;
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+import org.eclipse.core.resources.IMarker;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.ErrorDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.FontDialog;
+import org.eclipse.ui.*;
+import org.eclipse.ui.editors.text.TextEditor;
+import org.eclipse.ui.part.FileEditorInput;
+import org.eclipse.ui.part.MultiPageEditorPart;
+import org.eclipse.ui.ide.IDE;
+
+
+	public class ModelEditor extends TextEditor {
+
+		private ColorManager colorManager;
+
+		public ModelEditor() {
+			super();
+			colorManager = new ColorManager();
+			setSourceViewerConfiguration(new ModelConfiguration(colorManager));
+			setDocumentProvider(new ModelDocumentProvider());
+		}
+		public void dispose() {
+			colorManager.dispose();
+			super.dispose();
+		}
+
+	}
